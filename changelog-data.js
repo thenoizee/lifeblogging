ca028e79bec6135339dfd0ab52925cdd2c10f648
thenoizee <thesammyhubble@gmail.com>
2025-09-10 00:31:04 +0100

    v9.7.4-alpha
    In the Text/Merge Generator: Bug fix.

--- changelog-data.js
+++ changelog-data.js
@@ -2,6 +2,13 @@
 
 export const changelogData = [
     {
+        version: "v9.7.4-alpha",
+        date: "2025-09-10",
+        changes: [
+            "In the Text/Merge Generator: Bug fix.",
+        ]
+    },
+    {
         version: "v9.7.3-alpha",
         date: "2025-09-10",
         changes: [
