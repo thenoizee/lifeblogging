d615dd26ba488a6c1b3c871b4d8408e3907b59aa
thenoizee <thesammyhubble@gmail.com>
2025-11-28 22:31:58 +0000

    v9.36.1-alpha
    Bug fix 2.

---
+++ vidtrackr/api_keys.js
@@ -0,0 +1,11 @@
+// /trakter/api_keys.js
+// IMPORTANT: DO NOT COMMIT THIS FILE TO VERSION CONTROL.
+// 1. Create a new application on Trakt: https://trakt.tv/oauth/applications/new
+// 2. Set the "Redirect URI" to:
+//    - http://127.0.0.1:5500/trakter/ (for local testing, adjust port if needed)
+//    - https://lifeblogging.app/trakter/ (for production)
+//    You must add both URIs, one per line, in your Trakt application settings.
+// 3. Copy your Client ID and Client Secret from your Trakt application page into the variables below.
+
+// In api_keys.js
+export const TRAKT_CLIENT_ID = '46c509e2cef42228978ae6a69b138628a6399c0bf28b729b7883b19ab9b082eb';
