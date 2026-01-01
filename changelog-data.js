f18a94fe9b6dcf61979afde88555f53e90efb228
thenoizee <thesammyhubble@gmail.com>
2026-01-01 00:50:14 +0000

    v9.93.1-alpha
    Dashboard
    Fixed ISO 8601 date bug fix.

--- changelog-data.js
+++ changelog-data.js
@@ -2,6 +2,13 @@
 
 export const changelogData = [
     {
+    version: "v9.93.1-alpha",
+    date: "2026-01-01",
+    changes: [
+        "In Dashboard: Fixed ISO 8601 date bug fix.",
+    ]
+    },
+    {
     version: "v9.93.0-alpha",
     date: "2026-01-01",
     changes: [
