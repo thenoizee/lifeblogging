afeb168a2b043c47044fe5e1999221966fc3e5b8
thenoizee <thesammyhubble@gmail.com>
2026-04-14 20:43:55 +0100

    v14.30.0-alpha
    Hub
    Improved the login screen by adding a progress spinner to authentication buttons, locking input fields during login attempts, and enabling the 'Enter' key to submit credentials.

--- www/changelog-data.js
+++ www/changelog-data.js
@@ -2,6 +2,13 @@
 
 export const changelogData = [
     {
+    version: "v14.30.0-alpha",
+    date: "2026-04-14",
+    changes: [
+        "In Hub: Improved the login screen by adding a progress spinner to authentication buttons, locking input fields during login attempts, and enabling the 'Enter' key to submit credentials.",
+    ]
+    },
+    {
     version: "v14.29.1-alpha",
     date: "2026-04-14",
     changes: [
