3fad022cb3d38e3c139f9c2e23fe892050e15189
thenoizee <thesammyhubble@gmail.com>
2026-04-14 21:55:30 +0100

    v14.30.3-alpha
    Loggr
    Bug fix on mobile devices.

--- www/changelog-data.js
+++ www/changelog-data.js
@@ -2,6 +2,13 @@
 
 export const changelogData = [
     {
+    version: "v14.30.-alpha",
+    date: "2026-04-14",
+    changes: [
+        "In Loggr: Bug fix on mobile devices.",
+    ]
+    },
+    {
     version: "v14.30.2-alpha",
     date: "2026-04-14",
     changes: [
